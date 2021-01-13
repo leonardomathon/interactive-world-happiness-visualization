908345756d2c7976f398ff0d863cd83cc473f07f
leonardomathon <leonardo.mathon@hotmail.com>
2021-01-13 23:41:45 +0100

    Added css reset

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,7 @@
-import  * as d3 from 'd3';
+import * as d3 from 'd3';
 import * as topojson from 'topojson';
 import * as three from 'three';
 
 // Import stylesheet(s)
+import '../scss/reset.scss';
 import '../scss/style.scss';
-
-
