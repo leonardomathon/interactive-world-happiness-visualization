4bf5478107a3c003f9bc9e2404e8cc3fe26be283
leonardomathon <leonardo.mathon@hotmail.com>
2021-01-14 17:33:36 +0100

    Added json parser that generates clean json data set file

---
+++ scripts/json_parsing.py
@@ -0,0 +1,29 @@
+import itertools
+import json
+import os
+
+
+def by_year(data):
+    from itertools import groupby
+    from operator import itemgetter
+
+    retain_keys = ("Region", "Happiness Rank", "Happiness Score",
+                   "Economy (GDP per Capita)", "Freedom to make life choices", "Healthy life expectancy", "Generosity", "Trust (Government Corruption)")
+
+    for year, group in groupby(data, key=itemgetter("Year")):
+        as_tpl = tuple(group)
+        yield str(year), dict(zip(map(itemgetter("Country"), as_tpl), [{k: d[k] for k in retain_keys} for d in as_tpl]))
+
+
+script_dir = os.path.dirname(__file__)
+rel_path = "../datasets/cleaned-data.json"
+abs_file_path = os.path.join(script_dir, rel_path)
+
+with open(abs_file_path) as f:
+    data = json.load(f)
+
+rel_path = "../datasets/cleaned-data-formatted.json"
+abs_file_path = os.path.join(script_dir, rel_path)
+
+with open(abs_file_path, 'x') as outfile:
+    json.dump(dict(by_year(data)), outfile)
