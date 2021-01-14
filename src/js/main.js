f9e5610bbf92eb2a14acc237a2ee6d8e74453e68
leonardomathon <leonardo.mathon@hotmail.com>
2021-01-14 09:55:38 +0100

    Added d3 and three.js as well as an example showcasing a rotating sphere

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 import * as d3 from 'd3';
 import * as topojson from 'topojson';
 import * as three from 'three';
+import { init } from './sphere.js';
 
 // Import stylesheet(s)
-import '../css/reset.css';
 import '../css/style.css';
+
+init();
