98d07c7db4f9e746b20d8b11dfada4d89953b135
leonardomathon <leonardo.mathon@hotmail.com>
2021-01-15 19:20:21 +0100

    Added eventlistener to yearWorldHappiness variable to see changes made to that variable

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,49 @@
-import * as d3 from 'd3';
-import * as topojson from 'topojson';
-import * as three from 'three';
-import { init } from './sphere.js';
+import * as THREE from 'three';
+import * as topojson from 'topojson-client';
 
 // Import stylesheet(s)
 import '../css/style.css';
 
-init();
+// Import data sets
+import worldHappiness from '../../datasets/world-happiness.json';
+
+// <span> tag displaying selected year
+let yearText = document.getElementById('yearText');
+
+// <input> tag used for year selection
+let yearSlider = document.getElementById('yearSlider');
+
+// Variable that holds the current selected year (default is 2018)
+let yearSliderValue = 2018;
+
+// Object that holds the world happiness data from the selected year as yearWorldHappiness.data
+let yearWorldHappiness = {
+    dataInteral: worldHappiness[yearSliderValue],
+    dataListener: function (val) {},
+    set data(val) {
+        this.dataInteral = val;
+        this.dataListener(val);
+    },
+    get data() {
+        return this.dataInteral;
+    },
+    registerListener: function (listener) {
+        this.dataListener = listener;
+    },
+};
+
+// Event listener that listens to the range slider
+yearSlider.addEventListener('input', function (e) {
+    // Get slider value, update data and <span>
+    yearSliderValue = yearSlider.value;
+    yearWorldHappiness.data = worldHappiness[yearSliderValue];
+    yearText.innerHTML = yearSliderValue;
+});
+
+// Event listener that listens to the year data
+yearWorldHappiness.registerListener(function (val) {
+    console.log(
+        '%c Data has changed: year = ' + yearSliderValue,
+        'color:green; font-weight: 900;'
+    );
+});
